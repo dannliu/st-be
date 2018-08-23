667cd073c37858cefaa6f01f9c30eb0ba5ba8b94
czhao-aa <czhao@appannie.com>
2018-08-23 16:08:49 +0800

    update user model

--- colleague-api/app/models.py
+++ colleague-api/app/models.py
@@ -5,14 +5,37 @@ from datetime import datetime
 from app.extensions import db
 
 
+class UserStatus(object):
+    New = 0
+    Confirmed = 1  # after confirmed by sms
+    Blocked = 2
+    Deleted = 3
+
+
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, nullable=False, unique=True, autoincrement=True, primary_key=True)
-    user_name = db.Column(db.String(256), nullable=False)
-    password = db.Column(db.String(512), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    id = db.Column(db.BigInteger, nullable=False, unique=True, autoincrement=True, primary_key=True)
+    mobile = db.Column(db.String(50), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
 
-    @staticmethod
-    def get_user(user_id):
-        return User.find(user_id)
+    user_name = db.Column(db.String(256))
+    gender = db.Column(db.Integer)
+    avatar = db.Column(db.String(1024))
+
+    status = db.Column(db.Integer)
+
+    current_organization_id = db.Column(db.BigInteger, db.ForeignKey('organizations.id'))
+
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    last_login_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+class Organization(db.Model):
+    __tablename__ = 'organizations'
+
+    id = db.Column(db.BigInteger, nullable=False, unique=True, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(256))
+    icon = db.Column(db.String(1024))
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    verified = db.Column(db.Boolean)

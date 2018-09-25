16515876a64cf3a0e9cde27be602b91208a1843f
x4snowman <x4snowman@gmail.com>
2018-09-25 16:54:24 +0800

    fix minor bug

--- colleague-api/colleague/service/work_service.py
+++ colleague-api/colleague/service/work_service.py
@@ -5,11 +5,6 @@ from flask_jwt_extended import current_user
 
 
 def get_all_work_experiences(uid):
-    # todo we may need a server layer for this kind of api
-    work_experiences = WorkExperience.find_all_for_user(uid)
-    work_experiences.sort(cmp=cmp)
-    return [we.to_dict() for we in work_experiences]
-
     def cmp(w1, w2):
         f1 = w1.end_year + (w1.end_month or 1)
         f2 = w2.end_year + (w2.end_month or 1)
@@ -17,3 +12,8 @@ def get_all_work_experiences(uid):
             return (w1.update_date > w2.update_date) and -1 or 1
         else:
             return (f1 - f2) > 0 and -1 or 1
+
+    # todo we may need a server layer for this kind of api
+    work_experiences = WorkExperience.find_all_for_user(uid)
+    work_experiences.sort(cmp=cmp)
+    return [we.to_dict() for we in work_experiences]

1a58ccebcdd2fb1b5e5615f69da3a095dcf96315
st-danny <danyun.liu@shutu.tech>
2018-10-16 20:24:37 +0800

    add redis db dev

--- colleague-api/colleague/extensions.py
+++ colleague-api/colleague/extensions.py
@@ -7,7 +7,7 @@ from .config import settings
 db = SQLAlchemy()
 
 import redis
-redis_conn = redis.client.StrictRedis(host=settings['REDIS_HOST'], port=int(settings['REDIS_PORT']), db=0)
+redis_conn = redis.client.StrictRedis(host=settings['REDIS_HOST'], port=int(settings['REDIS_PORT']), db=settings['REDIS_DB'])
 
 
 from flask_jwt_extended import JWTManager
